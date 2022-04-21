dd2814b59951583e779956a135713013c1758d7a
jacoville <56603674+jacoville@users.noreply.github.com>
2022-04-21 17:07:39 -0600

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,10 +12,10 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-    }
+    
 
     private final String getMessage() {
-        return message;;
+        return message;
     }
 
 }
